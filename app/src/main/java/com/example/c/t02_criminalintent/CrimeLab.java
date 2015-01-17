311ceaff8f4b4e8db7132ed47f8c1a8469dd18ae
sunny <very_goody@nate.com>
2015-01-17 17:35:22 +0900

    add arryList

---
+++ app/src/main/java/com/example/c/t02_criminalintent/CrimeLab.java
@@ -0,0 +1,45 @@
+package com.example.c.t02_criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * Created by c on 2015-01-17.
+ */
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    private Context mAppContext;
+    private ArrayList<Crime> mCrimes;
+
+
+    public CrimeLab(Context appContext) {
+        mAppContext = appContext;
+        mCrimes = new ArrayList<Crime>();
+        for(int i=0; i<100; i++){
+            Crime c = new Crime();
+            c.setTitle("선희 #"+i);
+        }
+    }
+
+    public static CrimeLab get(Context c){
+        if(sCrimeLab == null){
+            sCrimeLab = new CrimeLab(c.getApplicationContext());
+        }
+        return sCrimeLab;
+    }
+
+    public ArrayList<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id){
+        for(Crime c : mCrimes){
+            if(c.getId().equals(id)){
+                return c;
+            }
+        }
+        return null;
+    }
+}
