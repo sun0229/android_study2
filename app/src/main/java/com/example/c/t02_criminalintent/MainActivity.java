33df5199fd9ed2b87524d54e0d6b0b05bd32f7e1
sunny <very_goody@nate.com>
2015-01-17 16:54:24 +0900

    commit2

--- app/src/main/java/com/example/c/t02_criminalintent/MainActivity.java
+++ app/src/main/java/com/example/c/t02_criminalintent/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.c.t02_criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +15,20 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fr = fm.findFragmentById(R.id.fragmentContainer);
+        if(fr == null){
+            fr = new CrimeFragment();
+
+            FragmentTransaction tr = fm.beginTransaction();
+            tr.add(R.id.fragmentContainer, fr)
+                    .commit();
+            //fm.beginTransaction()
+            //    .add(R.id.fragmentContainer, fr)
+            //    .commit();
+        }
+
     }
 
 
